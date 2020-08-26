f9dd825d89b9cb8762ecd0264d6b10b89b3774e1
GioMarcolla <gmarcolla@outlook.com>
2020-08-26 14:52:24 +0800

    Added popups to password field. Added colors to the popups

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { Segment, Form, Button, Popup } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
-
-
 class LoginForm extends Component {
     constructor(props) {
         super(props)
@@ -66,27 +64,53 @@ class LoginForm extends Component {
 
     }
 
-    helpEmail = <Popup
-        trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
-        content="email format: 'email@provider.sufix'"
-        on='focus' />
-
-    invalidEmail = <Popup
-        trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
-        content="invalid email, please make sure the format is correct."
-        on='focus' />
-
-    validEmail = <Popup
-        trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
-        content="This email seems to valid and not yet registered!"
-        on='focus' />
+    popups = {
+        email: {
+            help: <Popup
+                className="helper"
+                trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
+                content="email format: 'email@provider.sufix'"
+                on='focus' />,
+    
+            invalid: <Popup
+                className="warning"
+                trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
+                content="invalid email, please make sure the format is correct."
+                on='click' />,
+    
+            valid: <Popup
+                className="success"
+                trigger={<Form.Input type='text' fluid label='email' placeholder='email' id='loginInputEmail' onChange={this.validateEmail} />}
+                content="This email seems to valid and not yet registered!"
+                on='focus' />
+        },
+        password: {
+            help: <Popup
+                className="helper"
+                trigger={<Form.Input type='Password' fluid label='Password' placeholder='Password' id='loginInputPassword' onChange={this.validatePassword} />}
+                content={"Passwords must be at least 8 digits long and require at least: \n one(1) lowercase letter(a-z) \n one(1) uppercase letter(A-Z) \n one(1) number(0-9) \n one(1) special character('-', '_', '/', '!', '@', '#', '$', '%', '&', '*')"}
+                on='focus' />,
+    
+            invalid: <Popup
+                className="warning"
+                trigger={<Form.Input type='Password' fluid label='Password' placeholder='Password' id='loginInputPassword' onChange={this.validatePassword} />}
+                content="invalid password, please make sure to meet all requirements."
+                on='focus' />,
+    
+            valid: <Popup
+                className="success"
+                trigger={<Form.Input type='Password' fluid label='Password' placeholder='Password' id='loginInputPassword' onChange={this.validatePassword} />}
+                content="This password meets all requirements!"
+                on='focus' />
+        }
+    }
 
     render = () => {
 
         return <Segment id="loginContainer">
             <Form id="loginForm">
-                {this.state.emailLen === 0 ? this.helpEmail : this.state.validEmail ? this.validEmail : this.invalidEmail}
-                <Form.Input type='password' fluid label='password' placeholder='password' id='loginInputPassword' onChange={this.validatePassword} />
+                {this.state.emailLen === 0 ? this.popups.email.help : this.state.validEmail ? this.popups.email.valid : this.popups.email.invalid}
+                {this.state.passwordLen === 0 ? this.popups.password.help : this.state.validPassword ? this.popups.password.valid : this.popups.password.invalid}
             </Form>
             <Button fluid className={this.state.validEmail && this.state.validPassword ? "" : "disabled"} id="loginSubmit" type='submit'>Login</Button>
             <div id="loginFormOptions">
