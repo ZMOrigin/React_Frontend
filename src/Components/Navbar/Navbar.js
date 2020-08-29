c05becd37fbe6424dff0e2ba4a376328b4b5d8ee
GioMarcolla <gmarcolla@outlook.com>
2020-08-29 18:07:57 +0800

    New navBar, refactored navBar's CSS from App.css to Navbar.css

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { changePage } from '../../Redux/Actions/index'
+import Logo from '../../Assets/Images/ZM_Logo_White.png'
+import './Navbar.css'
 import {
     Menu,
     Segment,
     Image,
-    Search,
+    Button,
     Icon
 } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
@@ -24,28 +26,44 @@ const mapStateToProps = (state) => {
 }
 
 class Navbar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            openSearch: false
+        }
+    }
     handleClick = (e, { name }) => {
         this.props.changePage( name )
         // console.log(this.props.page)
     }
 
+    toggleSearch = (e) => {
+        this.setState({
+            search: !this.state.search
+        })
+    }
+
     render = () => {
-        return <Segment className='menuContainer'>
-            <Menu secondary className='container90'>
-                <Menu.Header as={Link} to='/Home'>
-                    <Image src={this.props.data.company.logo} alt='...' />
+        return <Segment id='menuContainer'>
+            <Menu secondary className='container84'>
+                <Menu.Header id="menuHeader" as={Link} to='/Home'>
+                    <Image src={Logo} alt='...' />
+                    <h1>Origin</h1>
                 </Menu.Header>
-                {this.props.data.menuItems.map((name) => {
-                    return <Menu.Item as={Link}
-                        key={name}
-                        className='ZMButton'
-                        name={name}
-                        onClick={this.handleClick}
-                        active={this.props.page === name}
-                        to={'/' + name} />
-                })}
                 <Menu.Menu className='floatRight'>
-                    <Search className='searchBox' placeholder='Search...' />
+                    {this.props.data.menuItems.map((name) => {
+                        return <Menu.Item as={Link}
+                            key={name}
+                            className='NavButton'
+                            name={name}
+                            onClick={this.handleClick}
+                            active={this.props.page === name}
+                            to={'/' + name} />
+                    })}
+                    <div id='navSearch' >
+                        <input className={this.state.search ? "searchInput" : "hiddenInput"}></input>
+                        <Icon className='searchIcon' size='large' name='search' onClick={this.toggleSearch} />
+                    </div>
                     <Menu.Item as={Link}
                         to='/User'
                         name='User'
