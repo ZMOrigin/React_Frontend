926d02b7e82c298b18f334080e725c6f650cb4fc
Giovanni Marcolla <gmarcolla@outlook.com>
2022-02-06 22:59:44 +0800

    reverted to Axios instead of Socket.io for static datas

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { setData, setUsers } from "./Redux/Actions/index"
 import { connect } from 'react-redux'
 //import getData from './Assets/data'
 import { socket } from './client'
+import axios from 'axios'
 
 let mapDispatchToProps = (dispatch) => {
     return {
@@ -28,11 +29,36 @@ let mapStateToProps = (state) => {
 class App extends React.Component {
     constructor(props) {
         super(props)
-        socket.emit("get", "appData", "5f466cc18596f6242caa02b4", (e, data) => {
+        const dataReqConfig = {
+            baseUrl: 'http://localhost:3131',
+            url: 'http://localhost:3131/appData',
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NDQxNTgwMjYsImV4cCI6MTY0NDI0NDQyNiwiYXVkIjoiaHR0cHM6Ly95b3VyZG9tYWluLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNjFmZWI2N2NiMWJjNzkxMTYwNmEwNDc0IiwianRpIjoiMjI1NWY2ZjItMDY2Zi00ZWE3LWEyZGYtNDMzMzc2ZjM5YmU4In0.92QpbxYqlM54y9hYwH9R7zPGTe6JeMm2cxHbmzOOLMI'
+            }
+        }
+        axios(dataReqConfig).then(res => {
+            const data = res.data.data[0]
             this.props.setData(data)
-            console.log("data: ")
-            console.log(data)
         })
+        const usersReqConfig = {
+            baseUrl: 'http://localhost:3131',
+            url: 'http://localhost:3131/users',
+            method: 'get',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NDQxNTgwMjYsImV4cCI6MTY0NDI0NDQyNiwiYXVkIjoiaHR0cHM6Ly95b3VyZG9tYWluLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNjFmZWI2N2NiMWJjNzkxMTYwNmEwNDc0IiwianRpIjoiMjI1NWY2ZjItMDY2Zi00ZWE3LWEyZGYtNDMzMzc2ZjM5YmU4In0.92QpbxYqlM54y9hYwH9R7zPGTe6JeMm2cxHbmzOOLMI'
+            }
+        }
+        axios(usersReqConfig).then(res => {
+            const users = res.data.data
+            console.log(users)
+            this.props.setUsers(users)
+        })
+        // socket.emit("get", "appData", "5f466cc18596f6242caa02b4", (e, data) => {
+        //     this.props.setData(data)
+        //     console.log("data: ")
+        //     console.log(data)
+        // })
 
         // socket.emit("get", "users", "5f43a76cf9827034dc5cc701", (e, data) => {
         //     this.props.setUsers(data)
@@ -54,7 +80,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -69,7 +95,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -84,7 +110,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -99,7 +125,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -114,7 +140,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -129,7 +155,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math", "blabla", "hello", "aloha", "sawadika"], 
+            specialty: ["English", "Math", "blabla", "hello", "aloha", "sawadika"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -144,7 +170,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -159,7 +185,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -174,7 +200,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Teacher",
-            specialty: ["English", "Math", "blabla", "hello", "aloha", "sawadika"], 
+            specialty: ["English", "Math", "blabla", "hello", "aloha", "sawadika"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -189,7 +215,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -204,7 +230,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -219,7 +245,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -234,7 +260,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -249,7 +275,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -264,7 +290,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -279,7 +305,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -294,7 +320,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }, {
             name: {
@@ -309,7 +335,7 @@ class App extends React.Component {
             country: "China",
             language: ["Chinese", "English"],
             role: "Student",
-            specialty: ["English", "Math"], 
+            specialty: ["English", "Math"],
             intro: "lorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem ilorem lorem i"
         }]
         this.props.setUsers(users)
